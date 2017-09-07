59feaba9126c739951aa7cf989e934d1ca54b5d9
Ruth Mesfun <ruth@flatironschool.com>
2017-09-07 00:56:57 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
 
   def add_student(student, grade)
     if @roster[grade].include?(student) == false
-      @roster[grade] << student
+      @roster[grade] = Array.new << student
     end
   end
 end
