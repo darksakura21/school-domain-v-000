89015e049a30cb8abde598d87b9652c2de8fd255
Ruth Mesfun <ruth@flatironschool.com>
2017-09-07 00:48:22 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,14 @@
 # code here!
 class School
-  attr_reader :name
-  attr_accessor :roster
-  
+  attr_reader :name, :roster
+  attr_accessor 
+
   def initialize(name)
     @name = name
     @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] = student
   end
 end
